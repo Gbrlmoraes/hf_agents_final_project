183f44796bba883251e96b8f9e91f9e9683a39ec
Gabriel Moraes <gabrielmoraesmagalhaes@gmail.com>
2025-05-09 22:26:16 -0300

    feat: Add new files for testing

--- resources/test_python.py
+++ resources/test_python.py
@@ -2,9 +2,9 @@ import random
 
 
 def get_mood() -> str:
-    moods = ["happy", "sad", "angry", "excited", "bored"]
+    moods = ['happy', 'sad', 'angry', 'excited', 'bored']
     return f"I'm feeling {random.choice(moods)} today!"
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     print(get_mood())
